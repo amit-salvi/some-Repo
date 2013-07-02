3975efc7a5e33f25ef0ffbfd26224395e58b1532
unknown <amsalvi@DIN15000131.corp.capgemini.com>
2013-07-02 11:13:15 +0530

    Initial commit... 3 files

---
+++ Somebean1.java
@@ -0,0 +1,12 @@
+package camelinaction;
+
+public class Somebean1 {
+	int i=0;
+	public String doSomething(String payload, String highPriority) {
+		i++;
+		System.out.println("I------------> " + i);
+		System.out.println("payload -----> " + payload);
+		System.out.println("high priority> " + highPriority);
+		return ""+i;
+	}
+}
